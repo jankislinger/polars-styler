0c4420485a73f539041250778339c90055c87770
Jan Kislinger <jan.kislinger@gmail.com>
2025-02-12 22:16:21 +0100

    run tests with multiple versions

--- checks.sh
+++ checks.sh
@@ -1,4 +1,11 @@
+__run_tests() {
+  uv run $1 $2 pytest && \
+  uv run $1 $2 python -m doctest polars_styler/**/*.py && \
+  echo $1 $2
+}
+
 uv run ruff format && \
   uv run ruff check --fix && \
-  uv run pytest && \
-  uv run python -m doctest polars_styler/**/*.py
+  __run_tests && \
+  __run_tests --with='polars~=1.10.0' && \
+  __run_tests --upgrade --prerelease=allow
