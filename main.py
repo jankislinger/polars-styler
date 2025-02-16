a9d25d56b321c2ff4a5b8c41cbda7a0edf724627
Jan Kislinger <jan.kislinger@gmail.com>
2025-02-16 09:37:37 +0100

    add spacing to the examples, refactor

--- main.py
+++ main.py
@@ -17,23 +17,19 @@ class Example:
 
 
 def main() -> None:
-    html = render_table([load_example(i) for i in range(3)])
+    html = render_examples([load_example(i) for i in range(3)])
     output_file = Path("output.html")
     output_file.write_text(html)
     print(f"Open in browser: file://{output_file.absolute()}")
 
 
 def load_example(i: int) -> Example:
-    path = EXAMPLES_DIR / f"t_{i:03d}.py"
+    code = EXAMPLES_DIR.joinpath(f"t_{i:03d}.py").read_text()
     module = importlib.import_module(f"polars_styler.examples.t_{i:03d}")
-    return Example(
-        title=f"Table {i:03d}",
-        code=path.read_text(),
-        table=module.styler.to_html(),
-    )
+    return Example(title=f"Table {i:03d}", code=code, table=module.styler.to_html())
 
 
-def render_table(examples):
+def render_examples(examples):
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template("template.html")
     return template.render(examples=examples)
