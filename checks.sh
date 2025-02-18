c9b3c3cb43eabeaa1adc4c05fde874bf85f7a9eb
Jan Kislinger <jan.kislinger@gmail.com>
2025-02-18 18:32:06 +0100

    add pytest.ini to enable doctests globally

--- checks.sh
+++ checks.sh
@@ -1,12 +1,6 @@
-__run_tests() {
-  uv run $1 $2 pytest && \
-  uv run $1 $2 python -m doctest polars_styler/**/*.py && \
-  echo $1 $2
-}
-
 uv run ruff format && \
   uv run ruff check --fix && \
-  __run_tests && \
-  __run_tests --with='polars~=1.10.0' && \
-  __run_tests --upgrade &&
+  uv run pytest && \
+  uv run --with='polars~=1.10.0' pytest && \
+  uv run --upgrade pytest &&
   uv run main.py
